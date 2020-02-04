b02890a7d88cb9a056a283dc9b3976fc6462a9fb
Samuel Tijani <omalitijani@gmail.com>
2020-02-04 13:56:06 +0100

    Fix #9

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -11,6 +11,24 @@ async function insert(user) {
     
 }
 
+async function getUserByEmailAndPassword(email, password) {
+    let user = await User.findOne({ email });
+
+    if (isUserValid(user, password, user.hashedPassword)) {
+        user = user.toObject();
+        delete user.hashedPassword;
+        return user;
+    } else {
+        return null;
+    }
+}
+
+
+function isUserValid(user, password, hashedPassword) {
+    return user && bcrypt.compareSync(password, hashedPassword);
+}
+
+
 module.exports = {
     insert
 };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const userCtrl = require('../controllers/user.controller');
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
+// localhost:3000/api/auth/register
 router.post('/register', asyncHandler(insert));
+router.post('/login', asyncHandler(getUserByEmailAndPassword));
 
 async function insert(req, res, next) {
     const user = req.body;
     console.log('registering user', user);
-    req.user = userCtrl.insert(user);
-    res.json(user);
+    const savedUser = await userCtrl.insert(user);
+    res.json(savedUser);
+}
+
+async function getUserByEmailAndPassword(req, res, next) {
+    const user = req.body;
+    console.log('searching user for', user);
+    const savedUser = await userCtrl.getUserByEmailAndPassword(
+        user.email,
+        user.password
+        );
+    res.json(savedUser);
 }
 
 
